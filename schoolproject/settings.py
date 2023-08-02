8bdbc628533bc87e231e98256864e4c8cae9a8e0
vishnudeviravi <ivishnudr@gmail.com>
2023-08-02 15:58:33 +0530

    ADDED HOME PAGE

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'schoolproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'schoolapp' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
